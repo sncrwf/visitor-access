e25730d86a8a5b4910bdce5e084873ecc2f2b4ef
Lucas Almeida <lucas.b.almeida@live.com>
2024-12-05 13:31:49 -0300

    disable cors

--- api-docs/swagger-initializer.js
+++ api-docs/swagger-initializer.js
@@ -4,6 +4,7 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: "../swagger.json",
+    enableCORS: false,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
